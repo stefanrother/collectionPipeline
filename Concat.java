7bd211e91a5da8ac1a282547a91f2470d5c6fbe5
Stefan Rother-Stübs <stefan_rother@gmx.de>
2021-11-21 16:26:50 +0100

    added second concat example

--- Concat.java
+++ Concat.java
@@ -11,4 +11,23 @@ clojure…
 
 */
 
-Stream.concat(Stream.of(1,2,3), Stream.of(4,5)).collect(Collectors.toList());
+Stream.concat(Stream.of(1,2,3), Stream.of(4,5))
+	.collect(Collectors.toList());
+
+/*
+
+ruby…
+
+[1,2,3].concat([4,5]).concat([6])
+# => [1, 2, 3, 4, 5, 6]
+
+clojure…
+
+(concat [1 2 3] [4 5] [6])
+;; => (1 2 3 4 5 6)
+
+*/
+Stream.concat(
+		Stream.of(1,2,3), Stream.of(4,5)
+	).concat(Stream.of(6))
+	.collect(Collectors.toList());
